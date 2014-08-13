96ed34936a6c3063b6c9acf7164f1a8231024089
vladimirantos <vladimirantos@seznam.cz>
2014-08-13 23:07:14 +0200

    Grafika webu

--- www/js/main.js
+++ www/js/main.js
@@ -4,3 +4,19 @@ function menu() {
     $(".menu").removeClass("active");
     $("#" + id).addClass("active");
 }
+function submenu(id) {
+    $("#" + id).show();
+}
+
+function hidesubmenu(id) {
+    $("#" + id).hide();
+}
+function change(id) {
+    var src = document.getElementById("" + id).getAttribute("src");
+    if (src == 'img/more.png') {
+        src = 'img/less.png';
+    } else {
+        src = 'img/more.png';
+    }
+    document.getElementById("" + id).setAttribute("src",src);
+}
